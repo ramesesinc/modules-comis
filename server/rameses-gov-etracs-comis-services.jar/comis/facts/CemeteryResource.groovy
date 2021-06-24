8e627c93ecca6ef2b422f2a25bff8fd2ea6d4cc9
Jessie Zamora <jzamss@gmail.com>
2021-06-24 20:09:19 +0800

    application: server: updates

--- server/rameses-gov-etracs-comis-services.jar/comis/facts/CemeteryResource.groovy
+++ server/rameses-gov-etracs-comis-services.jar/comis/facts/CemeteryResource.groovy
@@ -5,6 +5,9 @@ import java.util.*;
 public class CemeteryResource {
 	String objid;
 	String resourceid;
+	String cemeteryid;
+	String sectionid;
+	String blockid;
 	double areasqm;
 	
 	public CemeteryResource() {}
@@ -12,6 +15,9 @@ public class CemeteryResource {
 	public CemeteryResource(info) {
 		this.objid = info.objid;
 		this.resourceid = info.resource.objid;
+		this.cemeteryid = info.cemetery.objid;
+		this.sectionid = info.section.objid;
+		this.blockid = info.block.objid;
 		this.areasqm = info.areasqm;
 	}
 
